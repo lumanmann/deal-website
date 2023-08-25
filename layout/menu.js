42fd758b9f82e7c705f2d01a5c14361c84e0c5a5
john <aarontu803@gmail.com>
2023-08-25 23:02:43 +0800

    fix: menu width size in mobile

--- layout/menu.js
+++ layout/menu.js
@@ -4,17 +4,17 @@ const closeMenu = document.querySelector("#close-menu");
 
 openMenu.addEventListener("click", () => {
   mobileMenu.classList.remove("w-0");
-  mobileMenu.classList.add("w-screen");
+  mobileMenu.classList.add("w-full");
 });
 
 closeMenu.addEventListener("click", () => {
-  mobileMenu.classList.remove("w-screen");
+  mobileMenu.classList.remove("w-full");
   mobileMenu.classList.add("w-0");
 });
 
 window.addEventListener("resize", () => {
   if (window.innerWidth > 992) {
-    mobileMenu.classList.remove("w-screen");
+    mobileMenu.classList.remove("w-full");
     mobileMenu.classList.add("w-0");
   }
 });
